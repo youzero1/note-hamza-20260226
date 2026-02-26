e76a6b68f788ce89d5ad14ca8c5eb705d77f2b23
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-02-26 15:38:22 +0500

    Add entities/Note.ts

---
+++ entities/Note.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Note {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
